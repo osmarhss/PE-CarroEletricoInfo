d0e049f851a563eada283c3a086e86a46c6d5059
João Vitor Simão Pinheiro <joaovitor26.2002@gmail.com>
2024-05-01 17:41:20 -0300

    encapsulamento de cabeçalho
    separei a tag header em um arquivo js para o cabeçalho ser chamado em varias paginas sem a nescessidade de dar ctrl-c e ctrl-v varias vezes.

---
+++ cabecalho.js
@@ -0,0 +1,26 @@
+// cabecalho.js
+document.addEventListener("DOMContentLoaded", function() {
+    const cabecalhoContainer = document.getElementById("cabecalho-container");
+    cabecalhoContainer.innerHTML = `
+    <header>
+    <nav class="navbar bg-dark navbar-dark navbar-expand-sm sticky-top">
+        <div class="container">
+            <a href="" class="navbar-brand d-flex align-items-center">
+                <i class="bi-car-front-fill fs-1 me-2"></i><strong>DESCUBRA O FUTURO DA MOBILIDADE</strong></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menuNavbar">
+                <div class="navbar-nav ms-auto">
+                    <a href="#" class="nav-link">Cuidados essenciais</a>
+                    <a href="#" class="nav-link">Testemunhos </a>
+                    <a href="#" class="nav-link">FAQ</a>
+                    <a href="#" class="nav-link">Guia de modelos</a>
+                </div>
+            </div>
+
+        </div>
+    </nav>
+</header>
+    `;
+});
