4989435450c28532b397fdbb24ff0256a4aa4d3c
João Vitor Simão Pinheiro <joaovitor26.2002@gmail.com>
2024-06-06 16:43:21 -0300

    update

--- cabecalho.js
+++ cabecalho.js
@@ -5,17 +5,18 @@ document.addEventListener("DOMContentLoaded", function() {
     <header>
     <nav class="navbar bg-dark navbar-dark navbar-expand-sm sticky-top">
         <div class="container">
-            <a href="" class="navbar-brand d-flex align-items-center">
+            <a href="index.html" class="navbar-brand d-flex align-items-center">
                 <i class="bi-car-front-fill fs-1 me-2"></i><strong>DESCUBRA O FUTURO DA MOBILIDADE</strong></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="menuNavbar">
                 <div class="navbar-nav ms-auto">
+                    <a href="index.html" class="nav-link">Home</a>
                     <a href="#" class="nav-link">Cuidados essenciais</a>
                     <a href="#" class="nav-link">Testemunhos </a>
                     <a href="#" class="nav-link">FAQ</a>
-                    <a href="#" class="nav-link">Guia de modelos</a>
+                    <a href="listacarro.html" class="nav-link">Guia de modelos</a>
                 </div>
             </div>
 
